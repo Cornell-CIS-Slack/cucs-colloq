9b736d1a401b0b15df3756dcff43d9dad9b59cdd
Adrian Sampson <adrian@radbox.org>
2017-01-30 15:23:57 -0500

    Scrape Cornell site

--- colloq.py
+++ colloq.py
@@ -1,136 +1,91 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import lxml.etree
-import lxml.html
-import urllib
+import bs4
+import requests
 import re
-from datetime import datetime, timedelta
+from datetime import timedelta
 import dateutil.parser
 from icalendar import Calendar, Event, vText, vUri
-import paramiko
 import pytz
+import urllib.parse
+import sys
 
 
-FEED_URL = 'http://www.cs.washington.edu/events/colloquia-atom.xml'
-PAGE_URL = 'http://www.cs.washington.edu/htbin-post/mvis/mvis/Colloquia'
-COLLOQ_LENGTH = 60 # minutes
-DEST_PATH = 'public_html/colloquia.ics'
-BASE_HREF = 'http://www.cs.washington.edu'
-SFTP_SERVER = 'recycle.cs.washington.edu'
-TZINFO = pytz.timezone("America/Los_Angeles")
+PAGE_URL = 'https://www.cs.cornell.edu/events/colloquium'
+COLLOQ_LENGTH = 60  # minutes
+TZ = "America/New_York"
+TZINFO = pytz.timezone(TZ)
+CALNAME = 'Cornell CS Colloquia'
+LOCATION = 'Gates G01'
 
 
 def parse_date(date):
-    date = re.sub(r'-\d\d?:\d\d ', '', date) # remove time range
-    out = dateutil.parser.parse(date.decode('utf8'))
+    out = dateutil.parser.parse(date)
     out = out.replace(tzinfo=TZINFO)
     return out
 
 
-def make_event(title, date, stamp, location, link, speaker):
+def make_event(title, date, location, link, speaker):
     event = Event()
     event.add('summary', title)
     event.add('dtstart', date)
     event.add('dtend', date + timedelta(minutes=COLLOQ_LENGTH))
-    event.add('dtstamp', stamp)
     event['location'] = vText(location)
     event['uid'] = link
-    event['description'] = vText(speaker)
+    event['description'] = vText(speaker or 'Colloquium')
     event['URL'] = vUri(link)
     return event
 
 
-def event_id(url):
-    return re.search(r'\?[iI][dD]=(\d+)', link).group(1)
+def scrape(url):
+    """Get the event data from the public calendar URL.
+    """
+    req = requests.get(url)
+    soup = bs4.BeautifulSoup(req.content, "html.parser")
+    events_div = soup.find("div", class_="view-events")
+    for listing in events_div.find_all("div", class_="event-listing"):
+        # Extract the *easy* metadata.
+        title = listing.find(class_="event-title").get_text().strip()
+        date = listing.find(class_="date").get_text().strip()
+        time = listing.find(class_="time").get_text().strip()
+        link = listing.find(class_="event-title").find("a")['href']
+
+        # Some harder metadata is hidden in the strings.
+        text = listing.get_text()
+        match = re.search(r'Speaker:\s+(.*)', text)
+        speaker = match.group(1).strip() if match else None
+        match = re.search(r'Host:\s+(.*)', text)
+        host = match.group(1).strip() if match else None
+
+        yield {
+            'title': title,
+            'date': date,
+            'time': time,
+            'speaker': speaker,
+            'host': host,
+            'link': link,
+        }
 
 
 def colloq():
     cal = Calendar()
-    cal.add('prodid', '-//colloq.py//uwcse//')
+    cal.add('prodid', '-//colloq.py//cucs//')
     cal.add('version', '2.0')
-    cal.add('X-WR-CALNAME', 'UW CSE Colloquia')
-    cal.add('X-WR-TIMEZONE', 'America/Los_Angeles')
+    cal.add('X-WR-CALNAME', CALNAME)
+    cal.add('X-WR-TIMEZONE', TZ)
 
-    event_ids = set()
+    for event in scrape(PAGE_URL):
+        date = parse_date(event['date'])
 
-    tree = lxml.etree.parse(urllib.urlopen(FEED_URL))
-    root = tree.getroot()
-    for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
-        title = entry.find('{http://www.w3.org/2005/Atom}title').text
-        link = entry.find('{http://www.w3.org/2005/Atom}link').attrib['href']
-        speaker = (entry.find('{http://www.w3.org/2005/Atom}author')
-                        .find('{http://www.w3.org/2005/Atom}name').text)
-        stamp = entry.find('{http://www.w3.org/2005/Atom}updated').text
-        stamp = datetime.strptime(stamp, '%Y-%m-%dT%H:%M:%SZ')
-        stamp = stamp.replace(tzinfo=TZINFO)
+        cal.add_component(make_event(
+            event['title'],
+            date,
+            LOCATION,
+            urllib.parse.urljoin(PAGE_URL, event['link']),
+            event['speaker'],
+        ))
 
-        html = urllib.urlopen(link).read()
-        date, location = re.search(r'</b><br>([^<]+)<br>([^<]*)<p>', html).groups()
-        date = parse_date(date)
-
-        event_ids.add(event_id(link))
-
-        cal.add_component(make_event(title, date, stamp, location, link,
-                                     speaker))
-
-    page = urllib.urlopen(PAGE_URL).read()
-    root = lxml.html.fragment_fromstring(page, create_parent='xxx')
-    dts = root.xpath('.//dt')
-    dds = root.xpath('.//dd')
-    for i in range(len(dts)):
-        dt = dts[i]
-        dd = dds[i]
-
-        title = dt.xpath('./a')[0].xpath('string()')
-        link = dt.xpath('./a/@href')[0]
-        if '://' not in link:
-            link = BASE_HREF + link
-        speaker = dt.xpath('./b')[0].xpath('string()')
-        dt_parts = [s.strip() for s in dt.xpath('./text()')]
-        affiliation, date = [s for s in dt_parts if s]
-        if affiliation:
-            if affiliation.startswith('(') and affiliation.endswith(')'):
-                affiliation = affiliation[1:-1]
-            speaker += ', ' + affiliation
-        date = parse_date(date)
-        time, location = dd.xpath('./text()')[0].split(',', 1)
-        hour, minute = time.strip().split(':')
-        minute = minute.lower()
-        if minute.endswith('am') or minute.endswith('pm'):
-            if minute.endswith('pm'):
-                hour = int(hour) + 12
-                if hour == 24: # Noon special case.
-                    hour = 12
-            minute = minute[:-2]
-        date = date.replace(hour=int(hour), minute=int(minute))
-        location = location.strip()
-
-        if 'NO COLLOQUIUM SCHEDULED' in speaker:
-            continue
-
-        eid = event_id(link)
-        if eid in event_ids:
-            continue
-        else:
-            event_ids.add(eid)
-
-        stamp = datetime.now()
-        cal.add_component(make_event(title, date, stamp, location, link,
-                                     speaker))
-
-    sys.stdout.write(cal.to_ical())
-
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.connect(SFTP_SERVER)
-    try:
-        sftp = client.open_sftp()
-        f = sftp.open(DEST_PATH, 'w')
-        f.write(cal.to_ical())
-        f.close()
-        sftp.close()
-    finally:
-        client.close()
+    sys.stdout.buffer.write(cal.to_ical())
 
 
 if __name__ == '__main__':
